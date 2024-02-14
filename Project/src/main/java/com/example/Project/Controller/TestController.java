6e780a10e84d77092d7eaf7335485c1a4f2ec6bc
Volkane23 <mimizouta@gmail.com>
2024-02-14 23:19:13 +0100

    initializing resources

---
+++ Project/src/main/java/com/example/Project/Controller/TestController.java
@@ -0,0 +1,13 @@
+package com.example.Project.Controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+    @GetMapping("/")
+    public String home(Model model) {
+        return "";
+    }
+}
