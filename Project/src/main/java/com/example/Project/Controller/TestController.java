1ebaa58b7e28a8c62b2a880d5dd933cf31587ba7
Volkane23 <mimizouta@gmail.com>
2024-02-15 17:34:19 +0100

    responseFormChild page

--- Project/src/main/java/com/example/Project/Controller/TestController.java
+++ Project/src/main/java/com/example/Project/Controller/TestController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class TestController {
     @GetMapping("/")
     public String home(Model model) {
-        return "testimony.html";
+        return "responseFormChild.html";
     }
 }
