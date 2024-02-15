7e1fb541c85225005e69b22bd48d49eda808dc9a
Volkane23 <mimizouta@gmail.com>
2024-02-15 02:39:38 +0100

    added home page

--- Project/src/main/java/com/example/Project/Controller/TestController.java
+++ Project/src/main/java/com/example/Project/Controller/TestController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class TestController {
     @GetMapping("/")
     public String home(Model model) {
-        return "";
+        return "base.html";
     }
 }
