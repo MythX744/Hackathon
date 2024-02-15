2442672818b1b435353c8139baaff2590ec9c829
mythx <malakcristiano24@gmail.com>
2024-02-15 02:14:20 +0100

    login/signup

--- Project/src/main/java/com/example/Project/Controller/UserController.java
+++ Project/src/main/java/com/example/Project/Controller/UserController.java
@@ -1,5 +1,43 @@
 package com.example.Project.Controller;
 
 
+import com.example.Project.Model.User;
+import com.example.Project.Service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/User")
 public class UserController {
+
+    IUserService userService;
+
+    @Autowired
+    public UserController(IUserService userService) {
+        this.userService = userService;
+    }
+    @GetMapping("/loadLogin")
+    public String getLoginPage(Model leModel) {
+        User theUser = new User();
+        leModel.addAttribute("user", theUser);
+        return "login";
+    }
+    @PostMapping("/login")
+    public String login() {
+        return "redirect:/User/loadProfile";
+    }
+    @GetMapping("/loadSignup")
+    public String getSignUpPage(@ModelAttribute("user") User user){
+        return "signup";
+    }
+    @PostMapping("/signup")
+    public String saveUser(@ModelAttribute("user") User user, Model model){
+        userService.save(user);
+        return "redirect:/User/loadLogin";
+    }
 }
