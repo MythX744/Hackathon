4a5266c6aff2ebba2ddec490b589e116e08ddd8d
mythx <malakcristiano24@gmail.com>
2024-02-15 13:38:30 +0100

    header/footer/guest page

--- Project/src/main/java/com/example/Project/Controller/ResponseFormController.java
+++ Project/src/main/java/com/example/Project/Controller/ResponseFormController.java
@@ -1,10 +1,25 @@
 package com.example.Project.Controller;
 
-import jakarta.persistence.Column;
+import com.example.Project.Model.ResponseForm;
+import com.example.Project.Model.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
 
 @Controller
 @RequestMapping("/ResponseForm")
 public class ResponseFormController {
+
+    @GetMapping("/loadForm")
+    public String loadResponseForm(Model model) {
+        model.addAttribute("form", new ResponseForm());
+        return "responseForm";
+    }
+
+    @PostMapping("/submitResponse")
+    public String submitResponse() {
+        return "responseForm";
+    }
 }
