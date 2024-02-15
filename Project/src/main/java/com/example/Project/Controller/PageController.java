3006adb4d8fabba7af25d118125d2c37fd8e1666
mythx <malakcristiano24@gmail.com>
2024-02-15 19:48:06 +0100

    navigation

---
+++ Project/src/main/java/com/example/Project/Controller/PageController.java
@@ -0,0 +1,36 @@
+package com.example.Project.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/navigation")
+public class PageController {
+
+    @RequestMapping("/home")
+    public String LoadHome(Model model){
+        return "home";
+    }
+
+    @RequestMapping("/contact")
+    public String LoadContact(Model model){
+        return "";
+    }
+
+    @RequestMapping("/specialists")
+    public String LoadSpecialists(Model model){
+        return "";
+    }
+
+    @RequestMapping("/faq")
+    public String LoadFAQ(Model model){
+        return "";
+    }
+
+
+    @RequestMapping("/User/loadProfile")
+    public String LoadProfile(){
+        return "profile";
+    }
+}
